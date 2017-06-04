ee789115e3b9cd8d4b1f31c0fd807850ed7bd3b4
Mohamed Cherkaoui <chermed@gmail.com>
2017-06-04 22:53:06 +0200

    Stable version

---
+++ README.rst
@@ -0,0 +1,66 @@
+Dtools Package
+==============
+
+**Dtools** package provide a wrapper object arround relativedelta to manage date and datetime strings
+
+Examples :
+==========
+
+**Example 1:**
+
+.. code-block:: python
+
+    >>> from dtools import Date
+    >>> Date('2017-06-04').relativedelta(days=2)
+    '2017-06-06'
+    >>> type(Date('2017-06-04').relativedelta(days=2))
+    <class 'str'>
+
+
+**Example 2:**
+
+.. code-block:: python
+
+    >>> from dtools import Date
+    >>> Date('2017-06-04').relativedelta(month=2, day=3)
+    '2017-02-03'
+    >>> Date('2017-06-04').relativedelta(last_day=True)
+    '2017-06-30'
+    >>> Date('2017-06-04').relativedelta(months=4, last_day=True)
+    '2017-10-31'
+
+**Example 3:**
+
+.. code-block:: python
+
+    >>> from dtools import Date
+    >>> Date('2017-06-04').relativedelta(hour=12)
+    >>> '2017-06-04'
+    >>> Date('2017-06-04').relativedelta(hour=12, rtype='datetime')
+    >>> '2017-06-04 12:00:00'
+
+**Example 4:**
+
+.. code-block:: python
+
+    >>> from dtools import Date
+    >>> Date('2017-06-04 04:30:00').relativedelta(days=4, hour=12)
+    >>> '2017-06-08 12:30:00'
+    >>> Date('2017-06-04 04:30:00').relativedelta(days=4, hour=12, rtype='date')
+    >>> '2017-06-08'
+
+
+Licence
+=======
+
+This software is made available under the LGPL v3 license.
+
+Bug Tracker
+===========
+
+Please, feel free to report bugs or suggestions in the `Bug Tracker <https://github.com/chermed/dtools/issues>`_!
+
+Credits:
+========
+
+Mohamed Cherkaoui <http://mohamedcherkaoui.com>
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'future',
+    'python-dateutil',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='dtools',
+    version='1.0.0',
+    description="Python package to manage string date and datetime",
+    long_description=readme + '\n\n' + history,
+    author="Mohamed Cherkaoui",
+    author_email='chermed@gmail.com',
+    url='https://github.com/chermed/dtools',
+    packages=[
+        'dtools',
+    ],
+    package_dir={'dtools':
+                 'dtools'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='dtools',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
